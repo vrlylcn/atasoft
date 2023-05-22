abc82dc21f09bbb98161e9cafc8ed92c19e8237c
Vural Yalçın <vrlylcn@Vurals-MacBook-Pro.local>
2023-05-22 13:11:48 +0300

    Home slider alanı için owl carusel eklentisi, slider görselleri ve ikonları eklendi. Slider düzenlemesi .home-slider sınıfı içerisinde yapıldı. Slider alanı için gerekli olan JavaScript kodu yazıldı.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 $(function() {
-  console.log("App started..!");
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: [
+      '<img src="/images/icon-slider-prev.png" width="40" height="95" alt="Önceki Kampanya" />', 
+      '<img src="/images/icon-slider-next.png" width="40" height="95" alt="Sonraki Kampanya" />'
+    ]
+  });
 });
