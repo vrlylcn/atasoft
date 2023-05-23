e64eb521840377eb7ffa14c0a6b7944f30d49309
Vural Yalçın <vrlylcn@Vurals-MacBook-Pro.local>
2023-05-23 13:23:55 +0300

    Anasayfa slider alanının altında owl carousel kaynaklı oluşan boşluk JavaScript eklentisine dots:false parametresi eklenerek giderildi. Anasayfa ürün gruplarımız alanı için HTML, CSS kodu yazıldı. Kullanılan görseller eklendi.

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ $(function() {
     navText: [
       '<img src="/images/icon-slider-prev.png" width="40" height="95" alt="Önceki Kampanya" />', 
       '<img src="/images/icon-slider-next.png" width="40" height="95" alt="Sonraki Kampanya" />'
-    ]
+    ],
+    dots: false
   });
 });
