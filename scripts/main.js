1daf6ad2fff481ac2d4c104ce7f5b9c44ee1caf7
Vural Yalçın <vrlylcn@Vurals-MacBook-Pro.local>
2023-05-30 13:01:13 +0300

    Home slider alanı için kullanılan owl carousel eklentisi içerisindeki arrow image url düzenlendi.

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ $(function() {
     items: 1,
     nav: true,
     navText: [
-      '<img src="/images/icon-slider-prev.png" width="40" height="95" alt="Önceki Kampanya" />', 
-      '<img src="/images/icon-slider-next.png" width="40" height="95" alt="Sonraki Kampanya" />'
+      '<img src="images/icon-slider-prev.png" width="40" height="95" alt="Önceki Kampanya" />', 
+      '<img src="images/icon-slider-next.png" width="40" height="95" alt="Sonraki Kampanya" />'
     ],
     dots: false
   });
